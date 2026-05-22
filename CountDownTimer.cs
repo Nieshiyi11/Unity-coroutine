fb4c3d903c2f492847df4b122c58ecedb419ddd7
Nieshiyi11 <elevennie11@gmail.com>
2026-05-23 01:25:12 +0800

    协程 —— 倒计时

---
+++ CountDownTimer.cs
@@ -0,0 +1,26 @@
+public class CountdownTimer : MonoBehaviour
+{
+    public float duration = 10f;
+    void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    //协程方法
+    IEnumerator Countdown()
+    {
+        float remaining = duration;
+        while(remaining > 0f)
+        {
+            Debug.Log($"倒计时：{remaining:F1} 秒");
+            yield return new WaitForSeconds(1f);
+            remaining -= 1f;
+        }
+        Debug.Log("时间到。");
+    }
+  }
+  /*
+  $"倒计时: {remaining} 秒"       //输出: 倒计时: 9 秒
+  $"倒计时: {remaining:F1} 秒"    //输出: 倒计时: 9.0 秒
+  $"倒计时: {remaining:F2} 秒"    //输出: 倒计时: 9.00 秒
+  */
